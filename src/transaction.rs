6ac8641770bd5b4efc8d3c36fbe3124e18f86be9
Takashiidobe <idobetakashi@gmail.com>
2025-01-06 18:53:11 -0500

    start working on serializing transaction support

--- src/transaction.rs
+++ src/transaction.rs
@@ -5,7 +5,7 @@ use crate::{
     utils::{bytes_to_u16, bytes_to_u32},
 };
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub enum TransactionItem {
     Start(u32),          // start transaction $num
     Rollback(u32),       // rollback transaction $num
@@ -82,10 +82,12 @@ impl TransactionItem {
     pub fn from_bytes(bytes: &[u8]) -> Self {
         let marker = bytes[0];
 
+        let bytes = &bytes[1..];
+
         match marker {
-            0 => Self::Start(bytes_to_u32(&bytes[1..5])),
-            1 => Self::Rollback(bytes_to_u32(&bytes[1..5])),
-            2 => Self::Commit(bytes_to_u32(&bytes[1..5])),
+            0 => Self::Start(bytes_to_u32(&bytes[..4])),
+            1 => Self::Rollback(bytes_to_u32(&bytes[..4])),
+            2 => Self::Commit(bytes_to_u32(&bytes[..4])),
             3 => Self::Checkpoint,
             4 => Self::Insert(deserialize_bytes(bytes)),
             5 => Self::Delete(deserialize_bytes(bytes)),
@@ -95,9 +97,9 @@ impl TransactionItem {
 }
 
 fn deserialize_bytes(bytes: &[u8]) -> Vec<RowVal> {
-    let len = bytes_to_u16(&bytes[1..3]);
+    let len = bytes_to_u16(&bytes[0..2]);
     let mut items = vec![];
-    let mut i = 3;
+    let mut i = 2;
     for _ in 0..len {
         let row_type = RowType::from_bytes(&bytes[i..i + 1].try_into().unwrap());
         i += 1;
@@ -116,11 +118,70 @@ fn deserialize_bytes(bytes: &[u8]) -> Vec<RowVal> {
                 let len = bytes_to_u16(&bytes[i..i + 2]) as usize;
                 i += 2;
                 items.push(RowVal::Bytes(bytes[i..i + len].to_vec()));
+                i += len;
             }
             RowType::Bool => {
                 items.push(RowVal::Bool(bytes[i] == 1));
+                i += 1;
             }
         }
     }
     items
+}
+
+#[cfg(test)]
+mod tests {
+    use std::num::NonZeroU32;
+
+    use quickcheck::Arbitrary;
+    use quickcheck_macros::quickcheck;
+
+    use crate::row::{RowType, RowVal};
+
+    use super::TransactionItem;
+
+    impl Arbitrary for RowType {
+        fn arbitrary(g: &mut quickcheck::Gen) -> Self {
+            let choice = g.choose(&[0u8, 1, 2, 3]).unwrap();
+            match choice {
+                0 => RowType::Id,
+                1 => RowType::U32,
+                2 => RowType::Bool,
+                3 => RowType::Bytes,
+                _ => unreachable!(),
+            }
+        }
+    }
+
+    impl Arbitrary for RowVal {
+        fn arbitrary(g: &mut quickcheck::Gen) -> Self {
+            let row_type = RowType::arbitrary(g);
+            match row_type {
+                RowType::Id => RowVal::Id(NonZeroU32::arbitrary(g)),
+                RowType::U32 => RowVal::U32(u32::arbitrary(g)),
+                RowType::Bytes => RowVal::Bytes(Vec::arbitrary(g)),
+                RowType::Bool => RowVal::Bool(bool::arbitrary(g)),
+            }
+        }
+    }
+
+    impl Arbitrary for TransactionItem {
+        fn arbitrary(g: &mut quickcheck::Gen) -> Self {
+            let choice = g.choose(&[0u8, 1, 2, 3, 4, 5]).unwrap();
+            match choice {
+                0 => Self::Start(u32::arbitrary(g)),
+                1 => Self::Rollback(u32::arbitrary(g)),
+                2 => Self::Commit(u32::arbitrary(g)),
+                3 => Self::Checkpoint,
+                4 => Self::Insert(Vec::arbitrary(g)),
+                5 => Self::Delete(Vec::arbitrary(g)),
+                _ => unreachable!(),
+            }
+        }
+    }
+
+    #[quickcheck]
+    fn serde(transaction: TransactionItem) -> bool {
+        TransactionItem::from_bytes(&transaction.to_bytes()) == transaction
+    }
 }
